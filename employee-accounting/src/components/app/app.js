5c606ff62ded9794f73d045428581e00f2f58452
arcqkk5 <arcq-k5@yandex.ru>
2022-01-31 23:22:42 +0300

    add form and employees-list

--- employee-accounting/src/components/app/app.js
+++ employee-accounting/src/components/app/app.js
@@ -1,23 +1,29 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
+import "./app.css";
 
 function App() {
+  const data = [
+    { name: "Grigoriy", salary: 800, increase: true, like: false, id: 1 },
+    { name: "Egor", salary: 1000, increase: false, like: false, id: 2 },
+    { name: "Anton", salary: 1950, increase: false, like: false, id: 3 },
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
--- employee-accounting/src/components/employees-list-item/employees-list-item.js
+++ employee-accounting/src/components/employees-list-item/employees-list-item.js
@@ -1,26 +1,69 @@
+import { Component } from "react";
 import "./employees-list-item.css";
 
-const EmployeesListItem = () => {
-  return (
-    <li className="list-group-item d-flex justify-content-between">
-      <span className="list-group-item-label">Grigoriy Lafutkin</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue="1000$"
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      like: false,
+    };
+  }
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+
+  onLike = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+
+  render() {
+    const { name, salary } = this.props;
+    const { increase } = this.state;
+    const { like } = this.state;
+    let classNames = "list-group-item d-flex justify-content-between";
+    let classNamesForSpanName = "list-group-item-label";
+    if (like) {
+      classNamesForSpanName += " like";
+      console.log(classNamesForSpanName);
+      console.log(this.state);
+    }
+    if (increase) {
+      classNames += " increase";
+    }
+
+    return (
+      <li className={classNames}>
+        <span className={classNamesForSpanName} onClick={this.onLike}>
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + "$"}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.onIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
